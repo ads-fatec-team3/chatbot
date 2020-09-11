a4d19372fb70eced58541fade2a99a20aec32978
Eduardo Nunes <edugenix@gmail.com>
2020-09-11 06:33:35 +0000

    config da tabela users no bd

--- backend/src/main/java/br/gov/sp/fatec/backend/models/User.java
+++ backend/src/main/java/br/gov/sp/fatec/backend/models/User.java
@@ -1,12 +1,14 @@
 package br.gov.sp.fatec.backend.models;
 
 import javax.persistence.Column;
+import javax.persistence.Table;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
