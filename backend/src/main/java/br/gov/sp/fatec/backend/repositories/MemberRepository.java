56cc167de1be1f979d8bb4ff1446f55b0874c0ef
lebarreto <leticiambrt@gmail.com>
2020-09-23 12:25:11 +0000

    add @Transactional

--- backend/src/main/java/br/gov/sp/fatec/backend/repositories/MemberRepository.java
+++ backend/src/main/java/br/gov/sp/fatec/backend/repositories/MemberRepository.java
@@ -3,7 +3,13 @@ package br.gov.sp.fatec.backend.repositories;
 import br.gov.sp.fatec.backend.models.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findMemberById(long id);
+
+    @Query("select m from Member m where m.idUser = ?1")
+    public Member findMemberByUser(long idUser);
 }
