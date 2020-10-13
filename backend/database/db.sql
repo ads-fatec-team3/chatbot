7ef3808a7c1e275c9f11e29033e638d06bab97d5
Eduardo Nunes <edugenix@gmail.com>
2020-10-13 01:21:15 +0000

    script para criação dos bancos de dados

---
+++ backend/database/db.sql
@@ -0,0 +1,7 @@
+SELECT 'CREATE DATABASE gruly ENCODING "utf8"'
+WHERE NOT EXISTS
+(SELECT FROM pg_database WHERE datname = 'gruly')\gexec
+
+SELECT 'CREATE DATABASE gruly_test ENCODING "utf8"'
+WHERE NOT EXISTS
+(SELECT FROM pg_database WHERE datname = 'gruly_test')\gexec
