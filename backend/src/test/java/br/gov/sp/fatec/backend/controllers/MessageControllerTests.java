a6d4aaa747f0c301c9146deab4ab4f90a93b1c92
Eduardo Nunes <edugenix@gmail.com>
2020-10-15 05:24:54 +0000

    inclusão de testes para o MessageController

---
+++ backend/src/test/java/br/gov/sp/fatec/backend/controllers/MessageControllerTests.java
@@ -0,0 +1,111 @@
+package br.gov.sp.fatec.backend.controllers;
+
+import br.gov.sp.fatec.backend.models.Conversation;
+import br.gov.sp.fatec.backend.models.Member;
+import br.gov.sp.fatec.backend.models.Message;
+import br.gov.sp.fatec.backend.repositories.ConversationRepository;
+import br.gov.sp.fatec.backend.repositories.MemberRepository;
+import br.gov.sp.fatec.backend.repositories.MessageRepository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+@Transactional
+public class MessageControllerTests {
+  private final String BASE_API_MESSAGES_URL = "api/messages";
+
+  @Autowired
+  private MockMvc mockMvc;
+  
+  @Autowired
+  ObjectMapper objectMapper;
+
+  @Autowired
+  MessageRepository messageRepository;
+
+  @Autowired
+  MemberRepository memberRepository;
+  
+  @Autowired
+  ConversationRepository conversationRepository;
+
+  @Test
+  public void contextLoads() {}
+
+  @Test
+  public void insertMessage() throws Exception {
+    Member sender = new Member("sender", 10);
+    Conversation chat = new Conversation("chat");
+
+    memberRepository.save(sender);
+    conversationRepository.save(chat);
+
+    Message newMessage = new Message("test");
+
+    mockMvc.perform(
+      post("/{API_URL}", BASE_API_MESSAGES_URL)
+      .contentType(MediaType.APPLICATION_JSON)
+      .param("senderId", String.valueOf(sender.getId()))
+      .param("conversationId", String.valueOf(chat.getId()))
+      .content(objectMapper.writeValueAsString(newMessage)))
+      .andExpect(status().isCreated());
+  }
+
+  @Test
+  public void getMessage() throws Exception {
+    Message savedMessage = messageRepository.save(new Message("test"));
+    
+    mockMvc.perform(
+      get("/{API_URL}/{messageId}", BASE_API_MESSAGES_URL, savedMessage.getId())
+      .accept(MediaType.APPLICATION_JSON))
+      .andDo(print())
+      .andExpect(status().isOk())
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.data.text", is("test")));
+    }
+    
+  @Test
+  public void updateMessage() throws Exception {
+    Message updatedMessage = messageRepository.save(new Message("test"));
+    updatedMessage.setText("test updated");
+
+    mockMvc.perform(
+      put("/{API_URL}/{messageId}", BASE_API_MESSAGES_URL, updatedMessage.getId())
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(objectMapper.writeValueAsString(updatedMessage)))
+      .andExpect(status().isOk());
+
+    assertThat(updatedMessage.getText()).isEqualTo("test updated");
+  }
+
+  @Test
+  public void deleteMessage() throws Exception {
+    Message messageToDelete = messageRepository.save(new Message("test"));
+
+    mockMvc.perform(
+      delete("/{API_URL}/{memberId}", BASE_API_MESSAGES_URL, messageToDelete.getId()))
+      .andExpect(status().isOk());
+  }
+}
