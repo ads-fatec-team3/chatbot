9bbd72f600384ca0a58962bdc7df4b16d28f0ef5
LLBueno <leandro.bueno@znc.com.br>
2020-10-22 10:48:05 -0300

    Adicionadas mais respostas para o bot

--- artificial-intelligence/core/data.py
+++ artificial-intelligence/core/data.py
@@ -1,3 +1,17 @@
+from datetime import datetime
+
+
+def get_greeting():
+    current_hour = datetime.now().hour
+    if current_hour < 12:
+        return 'Bom dia!'
+    elif current_hour < 18:
+        return 'Boa tarde!'
+    else:
+        return 'Boa noite!'
+
+
+
 RESPONSES = [
     {
         'tags': ['oi', 'olá'],
@@ -14,5 +28,21 @@ RESPONSES = [
     {
         'tags': ['quem', 'gruly'],
         'responses': ['Eu sou o Gruly', 'Meu nome é Gruly', 'Gruly é o nome que meus criadores me deram']
+    },
+    {
+        'tags': ['agendamento', 'aula', 'marcar'],
+        'responses': ['Fale com um atendente na aba conversas!']
+    },
+    {
+        'tags': ['Obrigado', 'Valeu'],
+        'responses': ['De nada! ^-^', 'Estou aqui para ajudá-lo!']
+    },
+    {
+        'tags': ['remarcar', 'cancelamento', 'reagendar'],
+        'responses': ['Fale com um atendente na aba conversas!']
+    },
+    {
+        'tags': ['dia', 'tarde', 'noite'],
+        'responses': [get_greeting()]
     }
 ]
