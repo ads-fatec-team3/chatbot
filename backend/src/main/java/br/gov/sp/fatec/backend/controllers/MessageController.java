3bc90ee3d35ec94a8e17d3ca344ed4cc9df372dd
Eduardo Nunes <edugenix@gmail.com>
2020-10-28 02:14:20 +0000

    controllers usando services

--- backend/src/main/java/br/gov/sp/fatec/backend/controllers/MessageController.java
+++ backend/src/main/java/br/gov/sp/fatec/backend/controllers/MessageController.java
@@ -1,18 +1,16 @@
 package br.gov.sp.fatec.backend.controllers;
 
-import br.gov.sp.fatec.backend.exceptions.MessageException.MessageCrudException;
-import br.gov.sp.fatec.backend.exceptions.MessageException.MessageNotFoundException;
+import com.fasterxml.jackson.annotation.JsonView;
 
+import br.gov.sp.fatec.backend.exceptions.MessageException.MessageNotFoundException;
 import br.gov.sp.fatec.backend.models.Message;
-import br.gov.sp.fatec.backend.repositories.MessageRepository;
 import br.gov.sp.fatec.backend.services.MessageService;
 import br.gov.sp.fatec.backend.views.Views;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonView;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -34,73 +32,45 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class MessageController {
   @Autowired
-  private MessageRepository messageRepository;
-  
-  @Autowired
   private MessageService messageService;
 
   @JsonView(Views.DetailMessageView.class)
   @GetMapping
   @ApiOperation(value = "Retorna uma lista com os dados de todas as mensagens")
   public ResponseEntity<List<Message>> getAllMessages() {
-    List<Message> messages = messageRepository.findAll();
-
-    return ResponseEntity.ok(messages);
+    return ResponseEntity.ok(messageService.getAllMessages());
   }
 
   @JsonView(Views.DetailMessageView.class)
   @GetMapping("/{messageId}")
   @ApiOperation(value = "Retorna os dados de uma mensagem")
-  public ResponseEntity<Message> getMessageById(@PathVariable("messageId") long messageId) throws MessageNotFoundException {
-    Message fetchedMessage = messageRepository.findMessageById(messageId);
-
-    if(fetchedMessage == null) {
-      throw new MessageNotFoundException(messageId);
-    }
-
-    return ResponseEntity.ok(fetchedMessage);
+  public ResponseEntity<Message> getMessageById(@PathVariable("messageId") long messageId) {
+    return ResponseEntity.ok(messageService.getMessageById(messageId));
   }
 
   @PostMapping
   @ApiOperation(value = "Insere os dados de uma mensagem")
-  public ResponseEntity<Message> insert(@RequestBody Message message,
-                                        @RequestParam("senderId") long senderId,
-                                        @RequestParam("conversationId") long conversationId) {
+  public ResponseEntity<Message> createMessage(@RequestBody Message message,
+                                               @RequestParam("senderId") long senderId,
+                                               @RequestParam("conversationId") long conversationId) {
     messageService.createMessage(message, senderId, conversationId);
+
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
   @PutMapping("/{messageId}")
   @ApiOperation(value = "Atualiza os dados de uma mensagem")
-  public ResponseEntity<Message> update(@PathVariable("messageId") long messageId,
-                                        @RequestBody Message messageDataToUpdate) throws MessageNotFoundException {
-    Message message = messageRepository.findMessageById(messageId);
-
-    if(message == null) {
-      throw new MessageNotFoundException(messageId);
-    }
-
-    if(messageDataToUpdate.getText() != null) message.setText(messageDataToUpdate.getText());
-
-    Message updatedMessage = messageRepository.save(message);
-
-    if(updatedMessage == null) {
-      throw new MessageCrudException(String.format("erro ao atualizar os dados da mensagem de id = %d", messageId));
-    }
+  public ResponseEntity<Message> updateMessageById(@PathVariable("messageId") long messageId,
+                                               @RequestBody Message messageDataToUpdate) {
+    messageService.updateMessageById(messageId, messageDataToUpdate);
 
     return ResponseEntity.ok().build();
   }
 
   @DeleteMapping("/{messageId}")
   @ApiOperation(value = "Deleta os dados de uma mensagem")
-  public ResponseEntity<Message> delete(@PathVariable("messageId") long messageId) throws MessageNotFoundException {
-    Message messageToDelete = messageRepository.findMessageById(messageId);
-
-    if(messageToDelete == null) {
-      throw new MessageNotFoundException(messageId);
-    }
-
-    messageRepository.deleteById(messageId);
+  public ResponseEntity<Message> deleteMessageById(@PathVariable("messageId") long messageId) throws MessageNotFoundException {
+    messageService.deleteMessageById(messageId);
 
     return ResponseEntity.ok().build();
   }
