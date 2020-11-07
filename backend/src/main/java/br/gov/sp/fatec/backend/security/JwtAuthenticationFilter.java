a8ba5d89449462429e5f541ea485110e145e8c5f
rmsilva007 <rmsilva007@gmail.com>
2020-11-07 13:48:41 +0000

    classe de configuração filtro springsecurity

---
+++ backend/src/main/java/br/gov/sp/fatec/backend/security/JwtAuthenticationFilter.java
@@ -0,0 +1,48 @@
+package br.gov.sp.fatec.backend.security;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+
+//classe para melhor controle do filtro do Spring-security
+public class JwtAuthenticationFilter extends GenericFilterBean { // GenericFilterBean: filtro genérico do Spring
+
+    @Override                                                              //filtro chain p/ passar a requisição adiante
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException { 
+        
+        try {
+            HttpServletRequest servletRequest = (HttpServletRequest) request;//cast d/ requisição p/ HttpServletRequest
+            String authorization = servletRequest.getHeader(HttpHeaders.AUTHORIZATION); //get no conteúdo authorization do header 
+           
+            if (authorization != null) {
+            
+                Authentication credentials =  (Authentication) JwtUtils.parseToken(authorization.replaceAll("Bearer ", "")); // parse pra validar o token e loagar
+                SecurityContextHolder.getContext().setAuthentication(credentials);
+                 
+            }
+            
+            chain.doFilter(request, response); //vai pra rota
+        }
+        
+        catch(Throwable t) {
+            
+            HttpServletResponse servletResponse = (HttpServletResponse) response;
+            servletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, t.getMessage()); //token inválido: 401
+        }
+    }
+
+}
