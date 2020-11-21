04f54cd6ba90fa8cc69d4b9615bb983d9a822694
Bruna Totti <35302191+brunatotti@users.noreply.github.com>
2020-11-21 13:54:46 -0300

    Refatoraçao data.py

--- artificial-intelligence/core/data.py
+++ artificial-intelligence/core/data.py
@@ -3,39 +3,49 @@ RESPONSES = [
         'tags': ['oi', 'olá'],
         'responses': ['Olá, como posso ajudá-lo?', 'Oi!']
     },
+
     {
         'tags': ['trabalham', 'fazem'],
         'responses': ['A Anova ajuda pessoas com a auto escola']
     },
+
     {
         'tags': ['horário', 'funcionamento'],
         'responses': ['De segunda a sexta das 8h as 22h']
     },
+
     {
         'tags': ['quem', 'gruly'],
         'responses': ['Eu sou o Gruly', 'Meu nome é Gruly', 'Gruly é o nome que meus criadores me deram']
-    }
-    'tags': ['tarde', 'dia'],
+    },
+
+    {
+        'tags': ['tarde', 'dia'],
         'responses': ['olá, bom dia', 'ótimo dia!']
-    }
+    },
+
     {
-    'tags': ['boleto', 'segunda', 'via'],
-        'responses': ['procure por atendente no chat, ela te dara melhor essas informações']
-    }
+        'tags': ['boleto', 'segunda', 'via'],
+        'responses': ['Procure por atendente no chat, ela te dara melhor essas informações']
+    },
+
     {
-    'tags': ['atividades', 'atividade'],
-        'responses': ['procure pela aba atividade no chat!']
-    }
+        'tags': ['atividades', 'atividade'],
+        'responses': ['Procure pela aba atividade no chat!']
+    },
+
     {
-    'tags': ['pesquisar', 'pessoa'],
-        'responses': ['tente pesquisar a pessoa com quem deseja falar pelo nome no campo de buscas']
-    }
+        'tags': ['pesquisar', 'pessoa'],
+        'responses': ['Tente pesquisar a pessoa com quem deseja falar pelo nome no campo de buscas']
+    },
+
     {
     'tags': ['secretaria'],
-        'responses': ['procure por atendente no chat, ela te dara melhor essas informações']
-    }
+        'responses': ['Procure por atendente no chat, ela te dara melhor essas informações']
+    },
+
     {
     'tags': ['nova', 'conversa', 'conversas'],
-        'responses': ['se você deseja iniciar uma nova conversa, procure pelo botão que tem um "+" para isso']
-    }
+        'responses': ['Se você deseja iniciar uma nova conversa, procure pelo botão que tem um "+" para isso']
+    },
 ]
