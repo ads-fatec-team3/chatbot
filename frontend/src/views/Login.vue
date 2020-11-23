9a30580d7df43c0a508e4b182c916261af478074
lebarreto <leticiambrt@gmail.com>
2020-11-23 13:55:55 +0000

    Mostrar senha

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -12,7 +12,7 @@
           </v-col>
 
           <v-col cols='10' sm='6' md='3'>
-            <v-text-field label='Senha' type='password' v-model='password'></v-text-field>
+            <v-text-field label='Senha' :type="value ? 'password' : 'text'" v-model='password' :value="myPass" name="password" :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'" @click:append="() => (value = !value)"></v-text-field>
           </v-col>
           <v-col cols='10' sm='6' md='3'>
             <v-btn color="primary" x-medium block class="ma-2 pa-2" @click='submitLogin'>Entrar</v-btn>
@@ -29,7 +29,8 @@ export default {
   data () {
     return {
       username: null,
-      password: null
+      password: null,
+      value: String
     }
   },
   methods: {
