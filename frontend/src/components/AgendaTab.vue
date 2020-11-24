764f2f1bc66866b9c673875b12d2f623b619e89f
Bruna Totti <35302191+brunatotti@users.noreply.github.com>
2020-11-24 15:31:12 -0300

    Melhoria front

--- frontend/src/components/AgendaTab.vue
+++ frontend/src/components/AgendaTab.vue
@@ -3,66 +3,72 @@
     <div class="d-flex flex-row ma-4">
       <v-btn
         block
-        color="info"
+        color="primary"
         class="mx-2"
         @click="dialogChange"
       >
-        Adicionar
+        Nova Atividade
       </v-btn>
     </div>
-    <v-virtual-scroll
-      :items="agenda"
-      item-height="60"
-      class="scroll-box"
-    >
-      <template v-slot="{ item, index }">
 
-        <v-list-item :key="index">
+    <template>
+      <v-expansion-panels :items="agenda" class="scroll-box" >
+        <v-expansion-panel
+          v-for="(item,i) in agenda"
+          :key="i"
+        >
+          <v-expansion-panel-header>
+            <v-list-item-content>
+              <v-list-item-title class="d-flex flex-column">
+                <strong class="mb-1">{{ item.title|upperCase }}</strong>
+                <strong>{{ item.date_begin|formatDate }}</strong>
+              </v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn fab x-small depressed :color="item.color" />
+            </v-list-item-action>
 
-          <v-list-item-content>
-            <v-list-item-title class="d-flex flex-column">
-              <strong class="mb-1">{{ item.title|upperCase }}</strong>
-              <strong>{{ item.dateBegin|formatDate }}</strong>
-            </v-list-item-title>
-          </v-list-item-content>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+                <p><strong>Título: {{ item.title }}</strong></p>
+                <p><strong>Descrição: {{ item.description }}</strong></p>
+                <p><strong>Data Início: {{ item.date_begin|formatDate }}</strong></p>
+                <p><strong>Data Final: {{ item.date_end|formatDate }}</strong></p>
+                <p><strong>Prioridade: {{ item.color == 'gray' ? 'Baixa' : item.color == 'yellow' ? 'Média': 'Alta'}}</strong></p>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </template>
 
-          <v-list-item-action>
-            <v-btn fab small depressed :color="item.color" />
-          </v-list-item-action>
-
-        </v-list-item>
-        <v-divider></v-divider>
-
-      </template>
-    </v-virtual-scroll>
     <v-row justify="center">
       <v-dialog
         v-model="activeDialogAgenda"
         persistent
         max-width="370"
       >
+        <v-form ref="form">
         <v-card>
           <v-card-title class="headline">
-            Adicionar Atividade
+            Nova Atividade
           </v-card-title>
           <v-card-text>
-            <v-text-field label="Título" v-model="title"></v-text-field>
-            <v-textarea label="Descrição" rows="2" v-model="description"></v-textarea>
+            <v-text-field label="Título" v-model="title" :rules="[v => !!v || 'Título é obrigatório']" required></v-text-field>
+            <v-textarea label="Descrição" rows="2" v-model="description" :rules="[v => !!v || 'Descrição é obrigatória']" required></v-textarea>
             <v-row class="mx-1">
-              <v-text-field label="Data Início" type="date" class="mr-4" v-model="dateBegin"></v-text-field>
-              <v-text-field label="Hora Início" type="time" v-model="hourBegin"></v-text-field>
+              <v-text-field label="Data Início" type="date" class="mr-4" v-model="dateBegin" :rules="[v => !!v || 'Data de início é obrigatória']" required></v-text-field>
+              <v-text-field label="Hora Início" type="time" v-model="hourBegin" :rules="[v => !!v || 'Hora de início é obrigatória']" required></v-text-field>
             </v-row>
             <v-row class="mx-1">
-              <v-text-field label="Data Final" type="date" class="mr-4" v-model="dateEnd"></v-text-field>
-              <v-text-field label="Hora Final" type="time" v-model="hourEnd"></v-text-field>
+              <v-text-field label="Data Final" type="date" class="mr-4" v-model="dateEnd" :rules="[v => !!v || 'Data final é obrigatória']" required></v-text-field>
+              <v-text-field label="Hora Final" type="time" v-model="hourEnd" :rules="[v => !!v || 'Hora final é obrigatória']" required></v-text-field>
             </v-row>
-            <v-select v-model="selectedMembers" @change="sendMembers(selectedMembers)" multiple :items="members" label="Participantes" ></v-select>
+            <v-select v-model="selectedMembers" multiple :items="members" label="Participantes" :rules="[v => !!v || 'Membros são obrigatórios']" required></v-select>
 
-            <v-radio-group label="Prioridade" v-model="color">
+            <v-radio-group label="Prioridade" v-model="color" :rules="[v => !!v || 'Prioridade é obrigatória']" required>
               <v-row align="center" justify="center">
                 <v-radio label="Baixa" class="mr-2" color="green" value="green"></v-radio>
-                <v-radio label="Média" class="mr-2" color="orange" value="orange"></v-radio>
-                <v-radio label="Alta" class="mb-2" color="red" value="red"></v-radio>
+                <v-radio label="Média" class="mr-1" color="yellow" value="yellow"></v-radio>
+                <v-radio label="Alta" class="mb-2" color="orange" value="orange"></v-radio>
               </v-row>
             </v-radio-group>
           </v-card-text>
@@ -80,10 +86,11 @@
                 text
                 @click="createAgenda"
               >
-                Adicionar
+                Criar Atividade
               </v-btn>
             </v-card-actions>
         </v-card>
+        </v-form>
       </v-dialog>
     </v-row>
   </div>
@@ -109,15 +116,13 @@ export default {
       dateEnd: null,
       hourEnd: null,
       selectedMembers: [],
-      color: 'orange'
+      color: 'orange',
+      status: null
     }
   },
   methods: {
     dialogChange: function () {
       this.$emit('handleActiveDialog')
-    },
-    sendMembers: function (members) {
-      this.$emit('handleLoadMembers')
     },
     createAgenda: function () {
       const dateBegin = new Date(`${this.dateBegin} ${this.hourBegin}`)
@@ -130,6 +135,7 @@ export default {
         color: this.color
       }
       this.$emit('handleCreateAgenda', dataAgenda)
+      this.$refs.form.reset()
     }
   },
   filters: {
